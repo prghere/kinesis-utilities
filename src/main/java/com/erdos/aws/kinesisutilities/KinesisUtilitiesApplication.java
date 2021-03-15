a84af5e97ca2ffa586215e6c42564b6c0afc881d
prabhu <prghere@gmail.com>
2021-03-15 19:26:55 -0400

    kinesis utilities library, first commit

---
+++ src/main/java/com/erdos/aws/kinesisutilities/KinesisUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.erdos.aws.kinesisutilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesisUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesisUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erdos/aws/kinesisutilities/KinesisUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdos.aws.kinesisutilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesisUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
